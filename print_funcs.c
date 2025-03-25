31081e716d7e4716645217ba611663b48785f06d
Ravou <oliviaravou@hotmail.fr>
2025-03-25 17:21:39 +0100

    implement the function print_string to handle the specifier s

--- print_funcs.c
+++ print_funcs.c
@@ -8,3 +8,21 @@ int print_char(va_list args)
 {
         return (_putchar(c));
 }
+/**
+ * print_string - Function to display a string
+ * @arg: A va_list
+ *
+ * Return: The number of characters printed
+ */
+int print_string(va_list args)
+{
+	int count = 0;
+	
+	if (str == NULL)
+		str = "(null)";
+	while (*str)
+	{
+		count += _putchar(*str++);
+	}
+	return (count);
+}
