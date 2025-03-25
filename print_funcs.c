8ee0f34d93b00fcc163ff02d6b8b645f0cb8b86c
Ravou <oliviaravou@hotmail.fr>
2025-03-25 17:18:28 +0100

    implement the function print_char to handle the specifier c

--- print_funcs.c
+++ print_funcs.c
@@ -1,41 +1,10 @@
 #include "main.h"
 /**
- * print_char - Fonction pour afficher un caractère
- * @c: le caractère à afficher 
+ * print_char - Function to display a character
  *
- * Retour: Le nombre de caractère imprimés (ici, toujours 1)
+ * Return: The number of characters printed (always 1)
  */
 int print_char(va_list args)
 {
-	return (_putchar(c));
-}
-/**
- * print_string - Fonction pour afficher une chaine de caractères
- * @str: La chaine de caractère à afficher
- *
- * Retour: Le nombre de caractères imprimés
- */
-int print_string(va_list args)
-{
-	int count = 0;
-
-	if (str == NULL)
-		str = "(null)";
-	while (*str)
-	{
-		count += _putchar(*str++);
-	}
-	return (count);
-}
-/**
- * print_percentage - Handles the %% format specifier.
- * @args: A va_list (not used).
- *
- * Return: The number of characters printed (1).
- */
-int print_percentage(va_list args)
-{
-	(void)args;
-	_putchar('%');
-	return (1);
+        return (_putchar(c));
 }
