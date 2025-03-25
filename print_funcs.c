8b108d127f1ae2250db3f62d6b3e16829081d70c
Ravou <oliviaravou@hotmail.fr>
2025-03-25 16:24:05 +0100

    handle %c

---
+++ print_funcs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_char - Fonction pour afficher un caractère
+ * @c: le caractère à afficher 
+ *
+ * Retour: Le nombre de caractère imprimés (ici, toujours 1)
+ */
+int print_char(char c)
+{
+	return (_putchar(c));
+} 
