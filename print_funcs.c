f7e9e96eef71311bb1e60d8396fa6acb8f57862f
Ravou <oliviaravou@hotmail.fr>
2025-03-26 15:10:12 +0100

    update implement function _print_int_dec to handle printf

--- print_funcs.c
+++ print_funcs.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 /**
  * print_char - Function to display a character
  *
@@ -7,7 +8,9 @@
 int print_char(va_list args)
 {
 	char c = va_arg(args, int);
-        return (_putchar(c));
+
+	_putchar(c);
+        return (1);
 }
 /**
  * print_string - Function to display a string
@@ -42,34 +45,41 @@ int print_percentage(va_list args)
 }
 /**
  * print_int_dec - Function to display an integer in base 10
- * @n: The integer to be printed
+ * @input_number: The integer to be printed
  *
  * Return: The number of characters printed
  */
 int print_int_dec(va_list args)
 {
 	int count = 0;
-	int n = va_arg(args, int);
-	unsigned int num;
-	
-	if (n < 0)
-	{
-		count += _putchar('-');
-		num = -n;
-	}
-	else
-	{
-		num = n;
-	}
-	
+	int input_number = va_arg(args, int);
 	int divisor = 1;
-
-	while (num / divisor >= 10)
-		divisor *= 10;
-
-	for (; divisor > 0; divisor /= 10)
+	
 	{
-		count += _putchar((num / divisor) + '0');
-		num %= divisor;
+		if (input_number == INT_MIN)
+		{
+			count += _putchar('-');
+			count += _putchar('2');
+			input_number = 147483648;
+		}
+		else if (input_number < 0)
+		{
+			count += _putchar ('-');
+			input_number = -input_number;
+		}
 	}
+	
+	{
+		while (input_number / divisor >= 10)
+			divisor *= 10;
+	}
+
+	{
+		while (divisor > 0)
+		{
+			count += _putchar(( intput_number / divisor) % 10 + '0');
+			divisor /= 10;
+		}
+	}
+	return count;
 }
