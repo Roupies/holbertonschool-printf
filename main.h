df4f8eab5bb58859a5f30c6445e2a7d61364dce5
‘Roupies’ <maxime.naguet@gmail.com>
2025-03-26 16:45:26 +0100

    feat(main.h): add struct and function prototypes for _printf implementation

---
+++ main.h
@@ -0,0 +1,31 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * struct specifier_s - struct linking a format specifier to its function
+ * @specifier: The format specifier character (e.g., 'd', 's', 'c')
+ * @func: Pointer to the corresponding handler function
+ *
+ * Description: This structure is used to map format specifiers
+ * to their respective handling functions in the _printf implementation.
+ */
+typedef struct specifier_s
+{
+	char specifier;
+	int (*func)(va_list);
+} specifier_t;
+
+int _putchar(char c);
+
+int print_int_dec(va_list args);
+int print_string(va_list args);
+int print_char(va_list args);
+int print_percentage(va_list args);
+
+int _printf(const char *format, ...);
+
+#endif
