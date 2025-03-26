88c8d549082d6cdc558145717d8d383b58956a06
Ravou <oliviaravou@hotmail.fr>
2025-03-26 18:13:48 +0100

    again correction of betty

--- _putchar.c
+++ _putchar.c
@@ -1,4 +1,11 @@
 #include <unistd.h>
+
+/**
+ * _putchar - Writes a character to stdout.
+ * @c: The character to be written.
+ *
+ * Return: On success, returns the number of bytes written (1). On error, return -1 and sets errno appropriately.
+ */
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
